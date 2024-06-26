f5087cfa3ab8fca9db14e1d01ba0c577812a3075
Hyuuuka <86443416+hyuuuka@users.noreply.github.com>
2024-06-26 12:34:02 +0100

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,10 +1,6 @@
-const keep_alive = () => {
-
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.write("I'm alive");
   res.end();
 }).listen(8080);
-
-export { keep_alive };
